834e1204fe79fb8b0ebb4571e95c3e6bc3a1ce0e
rortegaa <rortegaa@everis.com>
2021-03-24 13:49:45 +0100

    Versión 1 - GIT

---
+++ src/com/everis/git/AppMain.java
@@ -0,0 +1,20 @@
+package com.everis.git;
+
+/**
+ * Formación GIT
+ * 
+ * @author rortegaa
+ *
+ */
+public class AppMain {
+
+	/**
+	 * Método Principal.
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		
+		System.out.println("Hola Mundo.");
+	}
+
+}
